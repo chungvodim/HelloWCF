e897770b55c3a938a0bb44a7da24024ce331c328
chungvodim <ba198647@gmail.com>
2016-01-11 19:07:11 +0800

    Init

---
+++ Service/Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ServiceModel.Samples
+{
+    class Service
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
