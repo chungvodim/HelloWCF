d98131db62f12f9617f987818e59b3002059cb9d
chungvodim <ba198647@gmail.com>
2016-01-13 11:22:33 +0800

    add

---
+++ RealNorthwindIISHost/IProductService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RealNorthwindIISHost
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IProductService" in both code and config file together.
+    [ServiceContract]
+    public interface IProductService
+    {
+        [OperationContract]
+        [FaultContract(typeof(ProductFault))]
+        Product GetProduct(int id);
+
+        [OperationContract]
+        [FaultContract(typeof(ProductFault))]
+        bool UpdateProduct(Product product, ref string message);
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "RealNorthwindService.ContractType".
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int ProductID { get; set; }
+        [DataMember]
+        public string ProductName { get; set; }
+        [DataMember]
+        public string QuantityPerUnit { get; set; }
+        [DataMember]
+        public decimal UnitPrice { get; set; }
+        [DataMember]
+        public bool Discontinued { get; set; }
+    }
+
+    [DataContract]
+    public class ProductFault
+    {
+        public ProductFault(string msg)
+        {
+            FaultMessage = msg;
+        }
+
+        [DataMember]
+        public string FaultMessage;
+    }
+}
---
+++ RealNorthwindIISHost/ProductService.svc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RealNorthwindIISHost
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ProductService : IProductService
+    {
+        public Product GetProduct(int id)
+        {
+            try
+            {
+                // TODO: call business logic layer to retrieve product
+                Product product = new Product();
+                product.ProductID = id;
+                product.ProductName =
+                "fake product name from service layer";
+                product.UnitPrice = 10.0m;
+                product.QuantityPerUnit = "fake QPU";
+                return product;
+                //throw new FaultException<ProductFault>(new ProductFault("fake"), "fake exception");
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+                string reason = "GetProduct Exception";
+                throw new FaultException<ProductFault>(new ProductFault(msg), reason);
+            }
+
+
+        }
+
+        public bool UpdateProduct(Product product, ref string message)
+        {
+            try
+            {
+                bool result = true;
+                // first check to see if it is a valid price
+                if (product.UnitPrice <= 0)
+                {
+                    message = "Price cannot be <= 0";
+                    result = false;
+                }
+                // ProductName can't be empty
+                else if (string.IsNullOrEmpty(product.ProductName))
+                {
+                    message = "Product name cannot be empty";
+                    result = false;
+                }
+                // QuantityPerUnit can't be empty
+                else if (string.IsNullOrEmpty(product.QuantityPerUnit))
+                {
+                    message = "Quantity cannot be empty";
+                    result = false;
+                }
+                else
+                {
+                    // TODO: call business logic layer to update product
+                    message = "Product updated successfully";
+                    result = true;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+                string reason = "UpdateProduct Exception";
+                throw new FaultException<ProductFault>(new ProductFault(msg), reason);
+            }
+        }
+
+    }
+}
